fb26ffebbc3d0318a6babc76ade56e4479c05ffa
Jonas Kau <j.kau@energieloft.de>
2019-02-05 12:25:56 +0100

    enhancement frontend and server

--- web/server.py
+++ web/server.py
@@ -1,13 +1,42 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, url_for, request, render_template, jsonify
+from werkzeug.utils import secure_filename
 
-@app.route('/', methods=['GET'])
-def hello_world():
+UPLOAD_FOLDER = '/pictures'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#STYLE = url_for('static', filename='style.css')
+
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict(id, permission):
-    return 'Hello World!!!!'
 
-if __name__ == '__main__':
-  app.run(host = '0.0.0.0', threaded=True)
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == 'POST':
+        file = request.files['picture']
+        (age, gender) = getPrediction(file)
+        return jsonify(age=age, gender=gender)
+
+@app.route('/save', methods=['GET', 'POST'])
+def save():
+    if request.method == 'POST':
+        save = request.form['save']
+        ID = request.form['ID']
+        if save == False:
+            deletePicture(ID)
+    return render_template('enddialog.html')
+        
+def getPrediction(file):
+    #send to dockercontainer, get result
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    (age, gender) = None, None
+    return (age, gender)
+
+def deletePicture(ID):
+    #delete Picture
+    return None
