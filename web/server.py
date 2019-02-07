25b512585e55f1e905b1445066fe4335b4f9840c
Jonas Kau <j.kau@energieloft.de>
2019-02-07 10:06:01 +0100

    enh picture saving, still not working though

--- web/server.py
+++ web/server.py
@@ -1,10 +1,13 @@
-import os, sys, base64
+import os, sys, base64, re, time
+from PIL import Image
+from io import BytesIO
 from flask import Flask, url_for, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 from keras.applications.vgg16 import vgg16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np 
+
 
 UPLOAD_FOLDER = '/pictures'
 app = Flask(__name__)
@@ -20,17 +23,20 @@ def index():
 @app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
     if request.method == 'POST':
-        base64String = request.files['picture']
-        print(base64String)
-        imageBinary = base64.decodestring(base64String)
+        base64String = request.form['image']
+        """imageBinary = base64.b64decode(base64String + "===")
         ID = getNextID()
-        img_path = os.getcwd()+ "/bilder/"+ID+"_.jpg"
+        img_path = os.getcwd()+ "\\bilder\\"+ str(ID) +"_.png"
+
         with open(img_path, 'wb') as f:
             f.write(imageBinary)
         img = image.load_img(img_path, target_size=(224, 224))
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+        x = preprocess_input(x)"""
+        ID = getNextID()
+        img_path = os.getcwd()+ "\\bilder\\"+ str(ID)
+        getI420FromBase64(base64String +"===",img_path)
 
         (age, gender) = getPrediction(x)
         return jsonify(age=age, gender=gender, id=ID)
@@ -67,3 +73,11 @@ def getNextID():
         if ID > nextID:
             nextID = ID
     return nextID + 1
+
+def getI420FromBase64(codec, image_path="c:\\"):
+    base64_data = re.sub('^data:image/.+;base64,', '', codec)
+    byte_data = base64.b64decode(base64_data)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    t = time.time()
+    img.save(image_path + str(t) + '.png', "PNG")
